4a2dc8337e39e8c17f310243beef8a20ee531b6b
yan <vampelf@gmail.com>
2026-07-19 17:51:42 +0900

    fix: resolve 'Page with redirect' indexing warning by forcing trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://vampelf.github.io',
   base: '/',
+  trailingSlash: 'always',
   integrations: [sitemap()],
 });
